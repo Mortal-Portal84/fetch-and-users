c234b55eaa7f31345855d7f522a04f5cebf9b8ff
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-01-28 13:26:59 +0300

    variables for peoject ready

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import './style.css'
 
+const appGrid: HTMLDivElement = document.getElementById('app')
+const card: HTMLDivElement = document.createElement('div')
+const title: HTMLHeadElement = document.createElement('h3')
+const text: HTMLParagraphElement = document.createElement('p')
+
+card.className = 'card'
+title.className = 'card__title'
+text.className = 'card__text'
+
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(json => console.log(json))
