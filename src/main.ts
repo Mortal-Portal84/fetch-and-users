3db2af2d3b7b3ef5e962e8024ab804b7275b7e81
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-01-28 16:25:23 +0300

    added Kishor's decision without typization

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,38 @@
 import './style.css'
 
-const appGrid: HTMLDivElement = document.getElementById('app')
-const card: HTMLDivElement = document.createElement('div')
-const title: HTMLHeadElement = document.createElement('h3')
-const text: HTMLParagraphElement = document.createElement('p')
+const appGrid = document.getElementById('app') as HTMLDivElement
 
-card.className = 'card'
-title.className = 'card__title'
-text.className = 'card__text'
-
-fetch('https://jsonplaceholder.typicode.com/posts')
+type Post = {
+  userId: number,
+  id: number,
+  title: string,
+  body: string
+}
+const getDataFromAPI = async (): Promise<Post[]> => fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
-  .then(json => console.log(json))
+  .then(json => json)
+
+const createCard = (object) => {
+  const card: HTMLDivElement = document.createElement('div')
+  card.className = 'card'
+  const title: HTMLHeadElement = document.createElement('h3')
+  title.className = 'card__title'
+  const text: HTMLParagraphElement = document.createElement('p')
+  text.className = 'card__text'
+
+  title.textContent = object.title
+  text.textContent = object.body
+
+  card.append(title, text)
+  return card
+}
+const appendPosts = async () => {
+  const posts = await getDataFromAPI()
+
+  posts.forEach((post) => {
+    const node = createCard(post)
+    appGrid.appendChild(node)
+  })
+}
+
+appendPosts()
